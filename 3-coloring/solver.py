5dd4031370f71a44d566bec48455886a3a024a79
Jean-Eudes Le Douget <jeaneudes.ledouget@gmail.com>
2020-05-30 15:47:29 +0200

    fix

--- 3-coloring/solver.py
+++ 3-coloring/solver.py
@@ -120,4 +120,4 @@ if __name__ == '__main__':
     else:
         print('This test requires an input file.  '
               'Please select one from the data directory. '
-              '(i.e. python solver.py ./data/ks_4_0)')
+              '(i.e. python solver.py ./data/gc_4_1)')
