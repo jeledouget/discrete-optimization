450c16175f7267de6fcfe7b39cdcac12c5eb9fca
Jean-Eudes Le Douget <jeaneudes.ledouget@gmail.com>
2020-06-09 19:12:50 +0200

    cleanup via inheritance

--- 3-coloring/nqueens.py
+++ 3-coloring/nqueens.py
@@ -172,112 +172,16 @@ class Nqueens:
                     return
 
 
-class NqueensQueue:
+class NqueensQueue(Nqueens):
 
     """ Based on rows as decision variables
      Here, no rollback: solution is abandoned if unfeasible
      2 options at each 'pick' based on domain
      """
 
-    def __init__(self, n=8, initials=None, plot=False, plot_latency=0.5, final_plot=False):
-        self.n = n
-        self.domain = np.ones((self.n, self.n), dtype=bool)
-        self.initials = initials
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.queue = []
-        self.plot = plot
-        self.final_plot = final_plot
-        self.picks = 0
-        self._solved = False
-        self.timing = dict()
-        self.plot_latency = plot_latency
-        if self.plot:
-            self.fig = plt.figure(figsize=(11, 6))
-            self.update_plot()
-
-    @property
-    def board(self):
-        a = self.domain.copy()
-        for i in range(self.n):
-            if a[i,:].sum() > 1:
-                a[i,:] = False
-        return a
-
-    @timing
-    def row_constraints_ok(self, row):
-        nb = self.domain[row,:].sum()
-        if nb == 1:
-            col = np.where(self.domain[row,:])[0][0]
-            return all([
-                self.domain[:,col].sum() == 1,
-                self.domain.diagonal(col - row).sum() == 1,
-                np.fliplr(self.domain).diagonal(self.n - 1 - col - row).sum() == 1
-            ])
-        elif nb == 0:
-            return False
-        else:
-            return True
-
-    @timing
-    def is_feasible(self):
-        feasible = all([self.row_constraints_ok(i) for i in range(self.n)])
-        return feasible
-
-    @timing
-    def is_solved(self):
-        if not self._solved:
-            solved = self.is_feasible() and (self.domain.sum() == self.n)
-            self._solved = solved
-        return self._solved
-
-    @timing
-    def update_plot(self):
-        if 'fig' not in self.__dict__:
-            self.fig = plt.figure(figsize=(11, 6))
-        self.fig.clf()
-        self.fig.suptitle(f'{self.n}-queens problem')
-        axes = self.fig.subplots(1, 2)
-        axes[0].set_title('Board', pad=20)
-        axes[1].set_title('Domain', pad=20)
-        for i in range(self.n):
-            axes[0].vlines(i + 0.5, ymin=-0.5, ymax=self.n - 0.5, lw=0.5)
-            axes[1].vlines(i + 0.5, ymin=-0.5, ymax=self.n - 0.5, lw=0.5)
-            axes[0].hlines(i + 0.5, xmin=-0.5, xmax=self.n - 0.5, lw=0.5)
-            axes[1].hlines(i + 0.5, xmin=-0.5, xmax=self.n - 0.5, lw=0.5)
-        axes[0].matshow(self.board)
-        axes[1].matshow(self.domain)
-        plt.draw()
-        if self.plot:
-            plt.pause(self.plot_latency)
-
-    @timing
-    def prune(self):
-        prune_again = True
-        while prune_again:
-            prune_again = False
-            for i in range(self.n):  # row
-                if self.domain[i,:].sum() == 1:
-                    col = np.where(self.domain[i,:])[0][0]  # only value
-                    if any([
-                        self.domain[:, col].sum() > 1,
-                        self.domain.diagonal(col - i).sum() > 1,
-                        np.fliplr(self.domain).diagonal(self.n - 1 - col - i).sum() > 1
-                    ]):
-                        prune_again = True
-                        self.domain[:, col] = False
-                        diag_indices = [
-                            [j, col - (i - j)]
-                            for j in range(self.n)
-                            if 0 <= col - (i - j) < self.n
-                        ] + [
-                            [j, col + (i - j)]
-                            for j in range(self.n)
-                            if 0 <= col + (i - j) < self.n
-                        ]
-                        self.domain[tuple(zip(*diag_indices))] = False
-                        # put back True value
-                        self.domain[i, col] = True
-            if self.plot:
-                self.update_plot()
 
     @timing
     def split(self):
