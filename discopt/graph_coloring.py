56e45f7b60941a17d670235129abed403d7ff4a3
Jean-Eudes Le Douget <jeaneudes.ledouget@gmail.com>
2020-06-12 18:27:21 +0200

    cleaunp

---
+++ discopt/graph_coloring.py
@@ -0,0 +1,8 @@
+"""
+Given a graph, color each node so that 2 connected nodes are colored differently.
+Minimize the number of colors used.
+===================================================================================== """
+
+
+class GraphColoring:
+    pass
